fe0818532bf82b4fc8410c51402defda25c44f36
Alex0494 <alessio.cozzolino@outlook.it>
2015-05-16 08:54:52 +0200

    new branch created. added publish script.

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='twitter_stream',
+    version='0.1',
+    description='Twitter Stream Listener',
+    author='Alex0494',
+    packages=find_packages(),
+)
