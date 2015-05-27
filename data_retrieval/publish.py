96350ba8dbc4b8a732c1d2fa8be2426a05944ca6
Alex0494 <alessio.cozzolino@outlook.it>
2015-05-27 16:39:43 +0200

    modified:   cleaning/publish.py 	modified:   data_retrieval/publish.py

--- data_retrieval/publish.py
+++ data_retrieval/publish.py
@@ -2,8 +2,6 @@ import botocore.session
 
 session = botocore.session.Session(session_vars={'profile': (None,   None, 'tsid-test')})
 s3 = session.create_client('s3', region_name='eu-west-1')
-archive = open('/home/alessio/Desktop/ProjectWork/coolest-programming-language/DataRetrieval/dist/twitter_stream-0.1.tar.gz', 'rb')
+archive = open('dist/twitter_stream-1.0.tar.gz', 'rb')
 s3.create_bucket(Bucket='tsid-maad-stream')
 s3.put_object(Bucket='tsid-maad-stream', ACL='public-read', Key='app.tar.gz', Body=archive)
-
-
