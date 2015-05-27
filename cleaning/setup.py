7dbe109ba9b869aff8d510c64ced565dc263ea7f
mv-ubuntu14 <angelo.calabria94@gmail.com>
2015-05-27 16:31:28 +0200

    new branch!

--- cleaning/setup.py
+++
@@ -1,9 +0,0 @@
-from setuptools import setup, find_packages
-
-setup(
-    name='cleaner',
-    version='1.0',
-    description='this library cleans the raw .json twitter data and isert them into a POSTGRESql DB via psycopg',
-    author='Alex0494',
-    packages=find_packages(),
-    )
--- data_retrieval/setup.py
+++
@@ -1,9 +0,0 @@
-from setuptools import setup, find_packages
-
-setup(
-    name='twitter_stream',
-    version='1.0',
-    description='Twitter Stream Listener',
-    author='Alex0494',
-    packages=find_packages(),
-)
